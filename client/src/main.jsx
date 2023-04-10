24c7cb4592410f1c74faa0f37eb167b8ba622e8f
Michael Bottom <mlbmike@gmail.com>
2023-04-10 01:22:59 -0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -19,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <App />
         </BrowserRouter>
       </React.StrictMode>
-    </ChatProvider>
+    </ChatProvider>,
   </Provider>,
 
 )
